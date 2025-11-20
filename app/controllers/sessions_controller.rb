7ff8b31d178f3dc032409f6860f629b2238857f7
Rahul <rahuldharan@hotmail.com>
2025-11-20 18:51:14 +0530

    Add SVG icons for analytics, emails, settings, and transactions
    - Introduced new SVG icons for analytics, emails, settings, and transactions to enhance the visual representation in the application.
    - Updated the navbar to include buttons for navigating to the respective sections using the newly added icons.
    - Improved user interface with consistent iconography for better user experience.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,9 @@
 class SessionsController < ApplicationController
   allow_unauthenticated_access only: %i[ new create ]
-  rate_limit to: 10, within: 3.minutes, only: :create, with: -> { redirect_to new_session_path, alert: "Try again later." }
+  rate_limit to: 10, within: 3.minutes, only:  %i[ create ], with: -> { redirect_to new_session_path, alert: "Try again later." }
 
   def new
+    redirect_to root_path if authenticated?
   end
 
   def create
